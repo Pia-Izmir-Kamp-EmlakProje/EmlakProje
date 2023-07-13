ac79f2afa8814c441bc632414d06964d6aaf12dc
ÖmerfarukYetim <omerfarukyetim@gmail.com>
2023-07-13 16:51:56 +0300

    Create Emlak Proje Java

---
+++ EmlakProje/src/test/java/com/emlakProje/EmlakProje/EmlakProjeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emlakProje.EmlakProje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmlakProjeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
