8ae3d369871cd4c1b3df457ce7c4fab85a0e4c62
mozdozor <muhammetay651@gmail.com>
2023-07-14 10:35:58 +0300

    third commit

--- EmlakProje/src/main/java/com/emlakProje/EmlakProje/EmlakProjeApplication.java
+++ EmlakProje/src/main/java/com/emlakProje/EmlakProje/EmlakProjeApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmlakProjeApplication {
 
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(EmlakProjeApplication.class, args);
 	}
