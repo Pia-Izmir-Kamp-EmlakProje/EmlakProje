2620259a52f297aa6b91d977548bd22b68855b5e
mozdozor <muhammetay651@gmail.com>
2023-07-14 11:37:11 +0300

    lastcommit

---
+++ src/main/java/com/emlakProje/EmlakProje/EmlakProjeApplication.java
@@ -0,0 +1,16 @@
+package com.emlakProje.EmlakProje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmlakProjeApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmlakProjeApplication.class, args);
+		System.out.println("denemeprint");
+
+	}
+
+}
